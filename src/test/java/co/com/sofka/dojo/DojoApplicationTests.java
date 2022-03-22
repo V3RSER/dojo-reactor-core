13f0dd778197bade5a4b37900a682676d46eee21
V3RSER <luisdeiko@outlook.com>
2022-03-22 08:57:49 -0500

    Proyecto generado

---
+++ src/test/java/co/com/sofka/dojo/DojoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
